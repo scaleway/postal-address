e394b9dcbe38cfb266b0487679edd344e719a053
Kevin Deldycke <kdeldycke@ocs.online.net>
2014-07-15 10:49:06 +0200

    Add module docstring.

--- address.py
+++ address.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+""" Utilities for address parsing and rendering. """
 
 from __future__ import (unicode_literals, print_function, absolute_import,
                         division)
