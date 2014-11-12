20aa158fb6fed83809ab42e539de57c3387aac8f
Kevin Deldycke <kdeldycke@ocs.online.net>
2014-11-12 12:06:02 +0100

    Reverts previous commit.

--- address.py
+++ address.py
@@ -334,6 +334,8 @@ def territory_tree(subdivision_code, include_country=True):
     while subdivision_code:
         subdiv = subdivisions.get(code=subdivision_code)
         yield subdiv
+        if not subdiv.parent_code:
+            break
         subdivision_code = subdiv.parent_code
 
     # Return country
