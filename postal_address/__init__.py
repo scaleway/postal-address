425e22a4f0b85362a63e5d91e137088c09f062bb
Kevin Deldycke <kdeldycke@ocs.online.net>
2014-12-18 18:19:11 +0100

    Post release version bump.

--- postal_address/__init__.py
+++ postal_address/__init__.py
@@ -17,4 +17,4 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-__version__ = '0.2.0'
+__version__ = '0.2.1.dev'
